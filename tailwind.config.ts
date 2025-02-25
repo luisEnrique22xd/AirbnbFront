cfc218b828d66ed17673f0f7b63a9440c5e6f408
Luis Enrique <luiskike2210@gmail.com>
2025-02-25 10:31:25 -0600

    Install and set up Nextjs, create project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      'airbnb': '#FF385C',
+      'airbnb-dark': '#FF385C',
       },
     },
   },
