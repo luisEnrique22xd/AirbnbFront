d2f6026902374359eeaee2ff4b706252cf430992
Luis Enrique <luiskike2210@gmail.com>
2025-03-03 19:45:46 -0600

    06 Landlord page

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
         <PropertyList />
         </div>
-        
+         
       </main>
   );
 }
