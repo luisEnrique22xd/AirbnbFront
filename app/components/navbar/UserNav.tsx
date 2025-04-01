074c16d4946882e0b865c6e769fbf26ad80f5306
Luis Enrique <luiskike2210@gmail.com>
2025-04-01 07:32:16 -0600

    Make conversations dynamic

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -34,6 +34,13 @@ const UserNav:React.FC<UserNavProps> =({userId})=>{
                 {userId ? (
                     <>
                         <MenuLink
+                        label="Inbox"
+                        onClick={()=>{
+                            setIsOpen(false);
+                            router.push('/inbox')
+                        }}
+                        />
+                        <MenuLink
                         label="My properties"
                         onClick={()=>{
                             setIsOpen(false);
