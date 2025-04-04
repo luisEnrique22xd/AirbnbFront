21264395681c47285a3cb13819947aa6b80d3067
Luis Enrique <luiskike2210@gmail.com>
2025-04-03 19:46:44 -0600

    Start new conversation

--- app/components/ContactButton.tsx
+++ app/components/ContactButton.tsx
@@ -1,6 +1,31 @@
-const ContactButton = () => {  
+'use client'
+import useLoginModal from "../hooks/useLoginModal";
+import { useRouter } from "next/navigation";
+import apiServices from "../services/apiServices";
+
+interface ContactButtonProps{
+    userId:string|null;
+    landlordId: string;
+}
+
+const ContactButton:React.FC<ContactButtonProps> = ({userId,landlordId}) => { 
+    const loginModal=useLoginModal() ;
+    const router = useRouter();
+    const startConversation = async()=>{
+        if(userId){
+            const conversation= await apiServices.get(`/api/chat/start/${landlordId}/`)
+            if(conversation.conversation_id){
+                router.push(`/inbox/${conversation.conversation_id}`)
+            }
+        }
+        else{
+            loginModal.open();
+        }
+    }
     return(
-        <div className="py-4 px-6 cursor-pointer bg-airbnb text-white rounded-xl hover:bg-airbnb-dark transition">
+        <div 
+        onClick={startConversation}
+        className="py-4 px-6 cursor-pointer bg-airbnb text-white rounded-xl hover:bg-airbnb-dark transition">
             Contact
         </div>
     )
